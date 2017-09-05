d5dd969dc23f243abe2551ea400fb9ca0ec582a9
leon0705 <490868429@qq.com>
2017-09-05 21:25:00 +0800

    重做首页，完成顶部以及搜索框，轮播图在做

--- src/js/epet_register/verify.js
+++ src/js/epet_register/verify.js
@@ -2,7 +2,7 @@
 * @Author: Marte
 * @Date:   2017-09-02 10:19:17
 * @Last Modified by:   Marte
-* @Last Modified time: 2017-09-05 14:59:49
+* @Last Modified time: 2017-09-05 15:22:41
 */
 // 表单验证
 $(document).ready(function(){
@@ -60,52 +60,7 @@ $(document).ready(function(){
         })
     })
 
-    function getlevel(){
-        var qiangdu = document.getElementById('qiangdu');
-        qiangdu.style.display = 'block';
-        $('#leval0').removeClass('defaultpwd');
-        $('#leval1').removeClass('defaultpwd');
-        $('#leval2').removeClass('defaultpwd');
-        var password = $('#passwordr').val();
-        var leval =  apsl(password);
-        if(password.length == 0 ){
-            return false;
-        }
-        for(var i = 0 ;i <= leval; i++){
-            $('#leval'+i).addClass('defaultpwd')
-        }
-    }
-    // 判断密码强度算法
-    function apsl(password) {
-        var securityLevelFlag = 0;
-        if (password.length < 6) {
-            return 0;
-        }
-        else {
-            var securityLevelFlagArray = new Array(0, 0, 0, 0);
-            for (var i = 0; i < password.length; i++) {
-                var asciiNumber = password.substr(i, 1).charCodeAt();
-                if (asciiNumber >= 48 && asciiNumber <= 57) {
-                    securityLevelFlagArray[0] = 1;  //digital
-                }
-                else if (asciiNumber >= 97 && asciiNumber <= 122) {
-                    securityLevelFlagArray[1] = 1;  //lowercase
-                }
-                else if (asciiNumber >= 65 && asciiNumber <= 90) {
-                    securityLevelFlagArray[2] = 1;  //uppercase
-                }
-                else {
-                    securityLevelFlagArray[3] = 1;  //specialcase
-                }
-            }
-            for (var i = 0; i < securityLevelFlagArray.length; i++) {
-                if (securityLevelFlagArray[i] == 1) {
-                    securityLevelFlag++;
-                }
-            }
-            return securityLevelFlag;
-        }
-    }
+    
     // $("#nickname").blur(function(){
     //     var name = document.getElementById('nickname').value;
     //     var nickname_img = document.getElementById('nickname_img');
